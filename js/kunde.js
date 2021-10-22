5d2545b1f9940278ce713d37df0d2e5a7e537a87
AnsCoding <78800508+AnsCoding@users.noreply.github.com>
2021-10-22 15:26:12 +0200

    Kundeside done

--- js/kunde.js
+++ js/kunde.js
@@ -2,3 +2,12 @@ const url = new URL(window.location.href);
 document.querySelector(".leje-bil").innerHTML = sessionStorage.getItem("bil");
 document.querySelector(".leje-dage").innerHTML = sessionStorage.getItem("dage");
 document.querySelector(".leje-pris").innerHTML = sessionStorage.getItem("pris");
+document.querySelector(".ekstra-udstyr").innerHTML = sessionStorage.getItem("ekstrasList");
+document.querySelector(".ekstra-pris").innerHTML = sessionStorage.getItem("ialt");
+
+let ialt = sessionStorage.getItem("ialt");
+let pris = sessionStorage.getItem("pris");
+
+const totalSum = Number(ialt) + Number(pris)
+
+document.querySelector(".ialt").innerHTML = totalSum;
