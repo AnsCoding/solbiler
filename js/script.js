96e58e6f51965902782dfa495ca4a5a5fc94cef8
AnsCoding <78800508+AnsCoding@users.noreply.github.com>
2021-10-22 15:57:46 +0200

    update new URL

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ function visBil(bilObject, lejePeriodeIdage){
     bilNode.querySelector(".kufferter").innerText = bilObject.kufferter;
     bilNode.querySelector(".pris").innerText = formatPris(lejePris);
 
-    const url = new URL("ekstra.html", window.location.href)
+    const url = new URL("ekstra.html", window.location)
     url.searchParams.append("bil", bilObject.navn);
     url.searchParams.append("dage", lejePeriodeIdage);
     url.searchParams.append("pris", lejePris);
